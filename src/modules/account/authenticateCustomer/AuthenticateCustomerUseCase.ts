dc12e85c2a67bec4a2a29400a3ec0d6d85a1b1f1
Herbert Vieira <herbert.vieira@clubpetro.com>
2022-04-07 07:46:30 -0300

    adds useCase and controller to authenticate customer

---
+++ src/modules/account/authenticateCustomer/AuthenticateCustomerUseCase.ts
@@ -0,0 +1,37 @@
+import { compare } from "bcrypt";
+import { sign } from "jsonwebtoken";
+import { prisma } from "../../../database/prismaClient";
+
+interface IAuthenticateCustomer {
+  username: string;
+  password: string;
+}
+
+export class AuthenticateCustomerUseCase {
+  async execute({ username, password }: IAuthenticateCustomer) {
+    const customer = await prisma.customers.findFirst({
+      where: {
+        username: {
+          mode: "insensitive",
+        },
+      },
+    });
+
+    if (!customer) {
+      throw new Error("Customers doesn't exists");
+    }
+
+    const passwordsMatch = await compare(password, customer.password);
+
+    if (!passwordsMatch) {
+      throw new Error("Invalid password");
+    }
+
+    const token = sign({ username }, "cd363b111009eaef1b3ce79dbc8fb12b", {
+      subject: customer.id,
+      expiresIn: "1d",
+    });
+
+    return { token };
+  }
+}
--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
+import { AuthenticateCustomerController } from "./modules/account/authenticateCustomer/AuthenticateCustomerController";
 import { CreateCustomerController } from "./modules/customers/useCases/createCustomer/CreateCustomerController";
 
 const routes = Router();
 
 const createCustomerController = new CreateCustomerController();
+const authenticateCustomerController = new AuthenticateCustomerController();
 
-routes.post("/customer/", createCustomerController.handle);
+routes.post("/auth/", authenticateCustomerController.handle);
+routes.post("/customer", createCustomerController.handle);
 
 export { routes };
